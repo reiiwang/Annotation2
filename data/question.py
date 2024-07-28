7703e4d3285a1aa90517badae84ff04cf1817232
reiiwang <sqq9458@gmail.com>
2024-07-29 01:24:51 +0800

    first commit

---
+++ data/question.py
@@ -0,0 +1,246 @@
+Contexts = [
+    { 
+        # 12,
+        "Question": "Can I prevent or reduce my risk of heart disease if I have diabetes? 如果我患有糖尿病，我可以預防或降低心臟病的風險嗎？",
+        "Evidence": [
+            "1. When you have diabetes, your chance of heart disease goes up, so you may need tests that check how well your ticker is working. 當您患有糖尿病時，您患心臟病的幾率會增加，因此您可能需要進行測試來檢查您的股票行情記錄器的工作情況。",
+            "2. They'll help your doctor see if you have signs of heart trouble and figure out the best treatment for you. 他們會幫助您的醫生查看您是否有心臟病跡象，並找出最適合您的治療方法。"
+        ],
+        "Answer": "Individuals with diabetes are at an increased risk of developing heart disease. Therefore, it is important for people with diabetes to undergo regular cardiac screenings to monitor their heart health and to receive appropriate treatment if needed. 糖尿病患者罹患心臟病的風險增加。因此，對於糖尿病患者來說，定期進行心臟檢查以監測其心臟健康狀況並在需要時接受適當的治療非常重要。"
+    },
+    { 
+        # 1045,
+        "Question": "What are the physical reasons for erectile dysfunction (ED)? 勃起功能障礙 (ED) 的生理原因是什麼？",
+        "Evidence": [
+            "1. Physical reasons for ED range from heart disease and diabetes to high blood pressure and obesity. ED 的身體原因包括心臟病、糖尿病、高血壓和肥胖等。",
+            "2. Damage to your nerves or arteries can cause problems with erections, too. 神經或動脈損傷也會導致勃起問題。",
+            "3. Lack of exercise, drinking, and smoking can lead to problems. 缺乏運動、飲酒和吸煙會導致問題。"
+        ],
+        "Answer": "Physical reasons for ED include heart disease, diabetes, high blood pressure, obesity, nerve or artery damage, lack of exercise, drinking, and smoking. ED 的身體原因包括心臟病、糖尿病、高血壓、肥胖、神經或動脈損傷、缺乏運動、飲酒和吸煙。"
+    },
+    { 
+        # 478,
+        "Question": "Who is most at risk for type 2 diabetes? 誰最容易罹患第 2 型糖尿病？",
+        "Evidence": [
+            "1. It doesn't handle insulin like it should. 它不能像應有的那樣處理胰島素。",
+            "2. This is a combination of conditions that put you at risk for other problems, like heart disease, diabetes, or stroke. 這是使您面臨其他問題風險的綜合病症，例如心臟病、糖尿病或中風。",
+            "3. For example, you might be overweight, especially around your waistline, and have high blood pressure, high blood sugar, as well as cholesterol problems. 例如，您可能會超重，尤其是腰圍附近，並且患有高血壓、高血糖以及膽固醇問題。",
+            "4. If you're overweight, the extra pounds put more pressure on your weight-bearing joints. 如果您超重，額外的體重會對您的承重關節造成更大的壓力。"
+        ],
+        "Answer": "People who are overweight, especially around their waistline, and have high blood pressure, high blood sugar, as well as cholesterol problems. 超重（尤其是腰圍超重）、患有高血壓、高血糖以及膽固醇問題的人。"
+    },
+    { 
+        # 83,
+        "Question": "What is the treatment for muscle pain from multiple sclerosis? 多發性硬化症引起的肌肉疼痛的治療方法是什麼？",
+        "Evidence": [
+            "1. Treatment: Your doctor may recommend pain relievers and drugs to ease muscle spasms. 治療：您的醫師可能會建議止痛藥和緩解肌肉痙攣的藥物。",
+            "2. Even Botox shots can help by temporarily paralyzing a muscle or nerve. 即使注射肉毒桿菌也可以暫時麻痺肌肉或神經。",
+            "3. Stretches and range-of-motion exercises may also help. 伸展運動和活動範圍練習也可能有所幫助。"
+        ],
+        "Answer": "Muscle pain from multiple sclerosis can be treated with pain relievers, drugs to ease muscle spasms, Botox shots, and stretches and range-of-motion exercises. 多發性硬化症引起的肌肉疼痛可以透過止痛藥、緩解肌肉痙攣的藥物、肉毒桿菌注射、伸展運動和活動範圍運動來治療。"
+    },
+    { 
+        # 1044,
+        "Question": "What is a dilated pupillary exam? 什麼是散瞳檢查？",
+        "Evidence": [
+            "1. The doctor uses special drops to expand your eye's pupil (he'll call this dilate). 醫生使用特殊的滴劑來擴大你眼睛的瞳孔（他稱之為擴張）。",
+            "2. That lets him check your retina for signs of disease. 這可以讓他檢查你的視網膜是否有疾病跡象。"
+        ],
+        "Answer": "A dilated pupillary exam is an eye exam in which the doctor uses special drops to expand the pupil (dilate it) so that he or she can check the retina for signs of disease. 散瞳檢查是一種眼科檢查，醫生使用特殊的眼藥水來擴大瞳孔（散瞳），以便檢查視網膜是否有疾病跡象。"
+    },
+    {
+        #  2001,
+        "Question": "How does cryolipolysis work to treat cellulite? 冷凍溶脂如何治療橘皮組織？",
+        "Evidence": [
+            "1. Once they're dead, your body naturally removes them. 一旦它們死了，你的身體就會自然地清除它們。",
+            "2. This treatment is usually used for body shaping, but fat removal can also improve your cellulite. 這種治療通常用於塑身，但去除脂肪也可以改善橘皮組織。",
+            "3. It can take three treatments and 3 or 4 months for you to see results. 可能需要 3 次治療和 3 或 4 個月的時間才能看到結果。",
+            "4. This fat removal procedure is often used on the thighs, buttocks, and abdomen. 這種脂肪去除手術通常用於大腿、臀部和腹部。"
+        ],
+        "Answer": "Cryolipolysis is a fat removal procedure that can improve cellulite. It works by freezing fat cells, which then die and are naturally removed by the body. The procedure typically takes three treatments and 3 or 4 months for results to be seen. It is often used on the thighs, buttocks, and abdomen. 冷凍溶脂是一種可以改善橘皮組織的脂肪去除程序。它的工作原理是冷凍脂肪細胞，然後脂肪細胞死亡並被身體自然去除。這個過程通常需要 3 次治療和 3 或 4 個月才能看到結果。常用於大腿、臀部和腹部。"
+    },
+    {
+        #  1025,
+        "Question": "How should I prepare for a parent-teacher conference if my child has ADHD? 如果我的孩子有過動症，我該如何準備家長會？",
+        "Evidence": [
+            "1. Be organized at meetings. 會議要有組織性。",
+            "2. Parent-teacher conferences are usually short, so come with a list of questions so you don't forget to ask something important. 家長會通常很短，所以請列出一系列問題，這樣您就不會忘記問一些重要的問題。",
+            "3. Organize report cards, test results, and teacher notes in a binder so they're at your fingertips. 將成績單、測驗結果和教師筆記整理在活頁夾中，以便觸手可及。"
+        ],
+        "Answer": "To prepare for a parent-teacher conference, it is important to be organized and come prepared with a list of questions. Additionally, it is helpful to organize report cards, test results, and teacher notes in a binder so they are easily accessible. 為了準備家長會，組織好並準備問題清單非常重要。此外，將成績單、測驗結果和教師筆記整理在活頁夾中也很有幫助，以便輕鬆存取。"
+    },
+    {
+        # 55,
+        "Question": "How can I get better sleep during cancer treatment? 在癌症治療期間如何獲得更好的睡眠？",
+        "Evidence": [
+            "1: A good night's sleep can help you feel more rested. 良好的睡眠可以幫助您感到更休息。",
+            "2: To help make that happen, don't get into your bed to read or work on your laptop. 為了幫助實現這一目標，請不要躺在床上閱讀或使用筆記型電腦工作。",
+            "3: Turn off music and all screens during bedtime, and limit daytime naps to an hour or less. 就寢時關閉音樂和所有螢幕，並將白天小睡限制在一個小時或更短的時間內。",
+            "4: Right before bed is also a good time for activities that relax you, like meditation or journaling. 睡前也是放鬆活動的好時機，例如冥想或寫日記。"
+        ],
+        "Answer": "To get better sleep during cancer treatment, avoid electronic devices before bed, limit daytime naps, and engage in relaxing activities before bed. 為了在癌症治療期間獲得更好的睡眠，睡前避免使用電子設備，限制白天小睡，並在睡前進行放鬆活動。"
+    },
+    {
+        # 47,
+        "Question": "How can diabetes cause polydipsia? 糖尿病如何引起煩渴？",
+        "Evidence": [
+            "1: Diabetes: Super high blood sugar will make you pee a lot. 糖尿病：超高血糖會讓你頻尿。",
+            "2: The more you pee, the more dehydrated and thirstier you get, and the more you drink. 尿越多，你就越脫水、越口渴，喝的水就越多。"
+        ],
+        "Answer": "Diabetes can cause polydipsia because high blood sugar levels lead to excessive urination, which in turn leads to dehydration and thirst. 糖尿病會引起煩渴，因為高血糖會導致排尿過多，進而導致脫水和口渴。"
+    },
+    {
+        # 467,
+        "Question": "How much calcium should I get per day? 我每天應該攝取多少鈣？",
+        "Evidence": [
+            "1. For calcium: Children ages 1-3 should get 700 milligrams of calcium a day. 鈣：1-3歲的兒童每天應攝取700毫克鈣。",
+            "2. Children ages 4-8 should get 1,000 milligrams per day. 4-8 歲的兒童每天應攝取 1,000 毫克。",
+            "3. Children over age 9 and teenagers should get 1,300 milligrams of calcium a day. 9歲以上兒童及青少年每天應攝取1,300毫克鈣。",
+            "4. Women over age 51 and men over age 71 should get 1,200 milligrams per day. 51 歲以上的女性和 71 歲以上的男性每天應攝取 1,200 毫克。",
+            "5. All other adults should get 1,000 milligrams per day. 所有其他成年人每天應攝取 1,000 毫克。"
+        ],
+        "Answer": "The recommended daily calcium intake is 700 milligrams for children ages 1-3, 1,000 milligrams for children ages 4-8, 1,300 milligrams for children over age 9 and teenagers, 1,200 milligrams for women over age 51 and men over age 71, and 1,000 milligrams for all other adults. 建議每日鈣攝取量為1-3歲兒童700毫克，4-8歲兒童1,000毫克，9歲以上兒童和青少年1,300毫克，51歲以上女性和71歲以上男性1,200毫克，1,000毫克對於所有其他成年人。"
+    },
+    {
+        # 426,
+        "Question": "How do you get anthrax? 怎樣得炭疽病？",
+        "Evidence": [
+            "1. You can't get it unless the spores get into your body, either through a cut or because you breathe them in or drink or eat something that's contaminated with spores that contain the bacteria. 除非孢子進入您的體內，無論是透過傷口還是因為您吸入它們，或喝或吃被含有細菌的孢子污染的東西，否則您不會感染它。",
+            "2. You cannot catch anthrax from someone else. 您無法從其他人那裡感染炭疽病。"
+        ],
+        "Answer": "Anthrax is a bacterial infection that can be contracted through contact with spores, either through a cut or by breathing them in, eating, or drinking something that is contaminated with spores. Anthrax cannot be transmitted from person to person. 炭疽是一種細菌感染，可以透過接觸孢子而感染，無論是透過割傷或吸入孢子、吃或喝被孢子污染的東西。炭疽病不能在人與人之間傳播。"
+    },
+    {
+        # 2017,
+        "Question": "How can sleep help sun-damaged skin? 睡眠如何幫助曬傷的皮膚？",
+        "Evidence": [
+            "1. Nighttime is when skin does the bulk of its repair work, such as making new cells and mending or shedding old, damaged ones. 夜間是皮膚進行大部分修復工作的時間，例如製造新細胞以及修復或脫落舊的、受損的細胞。",
+            "2. This is also when it makes the most of any creams and lotions you apply. 這也是皮膚充分利用您使用的面霜和乳液的時間。",
+            "3. Skin gets warmer at night, so products seep in better and yield faster results. 夜間皮膚會變暖，因此產品會更好地滲透並產生更快的效果。"
+        ],
+        "Answer": "During sleep, the skin repairs itself by producing new cells and shedding old, damaged ones. It also makes the most of any creams and lotions applied, as the skin is warmer at night and products seep in better. 在睡眠期間，皮膚透過產生新細胞並脫落舊的、受損的細胞來自我修復。它還可以充分利用所使用的乳霜和乳液，因為夜間皮膚會更溫暖，產品會更好地滲透。"
+    },
+    {
+        # 1007,
+        "Question": "How do you know if you have a broken shoulder? 如何知道自己的肩膀是否骨折？",
+        "Evidence": [
+            "1. First, your doctor will carefully check the injury to see where and how bad it is. 首先，你的醫生會仔細檢查傷口，看看傷勢在哪裡以及有多嚴重。",
+            "2. So expect a physical exam and an X-ray. 因此，請做好身體檢查和 X 光檢查。",
+            "3. After that, you may need to get a CT scan, which is a powerful X-ray that makes detailed pictures inside your body. 之後，您可能需要進行 CT 掃描，這是一種強大的 X 射線，可以拍攝您體內的詳細圖片。",
+            "4. Your doctor will then suggest a treatment plan. 然後你的醫生會建議治療計劃。"
+        ],
+        "Answer": "To determine if you have a broken shoulder, your doctor will conduct a physical exam and order an X-ray. If the X-ray shows a break, you may need to undergo a CT scan to get a more detailed picture of the injury. Your doctor will then develop a treatment plan based on the results of your imaging tests. 為了確定您的肩膀是否骨折，您的醫生會進行身體檢查並安排 X 光檢查。如果 X 光檢查顯示破損，您可能需要進行 CT 掃描以獲得更詳細的受傷情況。然後，您的醫生將根據您的影像學檢查結果制定治療計劃。"
+    },
+    {
+        # 1000,
+        "Question": "What else matters in a paleo autoimmune protocol for rheumatoid arthritis? 類風濕性關節炎的古自體免疫方案還有什麼重要的？",
+        "Evidence": [
+            "1. AIP isn't just about food. AIP 不僅僅是食物。",
+            "2. Your lifestyle choices also matter. 您的生活方式選擇也很重要。",
+            "3. Getting the sleep you need, managing stress, exercising, and staying socially active are all important. 獲得所需的睡眠、控制壓力、運動和保持社交活躍都很重要。"
+        ],
+        "Answer": "In addition to diet, lifestyle choices also play a role in the Paleo Autoimmune Protocol (AIP) for rheumatoid arthritis. Getting enough sleep, managing stress, exercising, and staying socially active are all important aspects of this holistic approach to healing. 除了飲食之外，生活方式的選擇也在類風濕性關節炎的古自體免疫方案（AIP）中發揮作用。充足的睡眠、控制壓力、運動和保持社交活躍都是這種整體治療方法的重要方面。"
+    },
+    {
+        # 22,
+        "Question": "What are rectal suppositories used for? 直腸栓劑有什麼用途？",
+        "Evidence": [
+            "1. Rectal suppositories go in your bottom. 將直腸栓劑放入臀部。",
+            "2. They are about an inch long and have a rounded or bullet-shaped tip. 它們大約一英吋長，有一個圓形或子彈形的尖端。",
+            "3. You might take them to treat: Allergies Anxiety Asthma Constipation Fever Hemorrhoids Motion sickness Nausea Pain and itching Mental health problems, such as schizophrenia or bipolar disorder Vaginal suppositories are oval-shaped. 您可能會服用它們來治療： 過敏 焦慮 氣喘 便秘 發燒 痔瘡 暈車 噁心 疼痛和搔癢 精神健康問題，例如精神分裂症或躁鬱症 陰道栓劑是橢圓形的。"
+        ],
+        "Answer": "Rectal suppositories are used to treat a variety of conditions, including allergies, anxiety, asthma, constipation, fever, hemorrhoids, motion sickness, nausea, pain and itching, and mental health problems. 直腸栓劑用於治療多種病症，包括過敏、焦慮、氣喘、便秘、發燒、痔瘡、暈動病、噁心、疼痛和搔癢以及心理健康問題。"
+    },
+    {
+        # 44,
+        "Question": "What is abdominal migraine like in children? 兒童腹部偏頭痛是什麼樣的？",
+        "Evidence": [
+            "1. As their name suggests, they make your belly ache instead. 正如他們的名字所暗示的，他們反而會讓你肚子痛。",
+            "2. Your little one could also: Feel queasy or throw up Be pale or flushed Yawn, be drowsy, or have little energy Lose their appetite or be unable to eat Have dark shadows under their eyes Abdominal migraines are often sudden and quite severe. 您的孩子也可能會： 感到噁心或嘔吐 蒼白或潮紅 打哈欠、昏昏欲睡或精神不振 食慾不振或無法進食 眼睛下方有黑影 腹部偏頭痛通常是突然發生的，而且相當嚴重。"
+        ],
+        "Answer": "Abdominal migraine in children is characterized by stomach ache, nausea, vomiting, pale or flushed skin, yawning, drowsiness, loss of appetite, and dark shadows under the eyes. 兒童腹性偏頭痛的特徵是胃痛、噁心、嘔吐、皮膚蒼白或潮紅、打哈欠、嗜睡、食慾不振、眼睛下方有黑影。"
+    },
+    {
+        # 37,
+        "Question": "What are the risks of immunotherapy when treating cancer? 治療癌症時免疫療法有哪些風險？",
+        "Evidence": [
+            "1. The area where the medication goes into your body could hurt, itch, swell, turn red, or get sore. 藥物進入體內的區域可能會疼痛、發癢、腫脹、變紅或酸痛。",
+            "2. Most of the time, these ease up after your first treatment. 大多數時候，第一次治療後這些症狀會緩解。",
+            "3. It isn't a quick fix. 這不是一個快速解決辦法。"
+        ],
+        "Answer": "There are risks to immunotherapy, including pain, swelling, and redness at the injection site. These side effects usually go away after the first treatment. Immunotherapy is not a quick fix and may take time to work. 免疫療法有風險，包括注射部位疼痛、腫脹和發紅。這些副作用通常會在第一次治療後消失。免疫療法不是一個快速解決方案，可能需要時間才能發揮作用。"
+    },
+    {
+        # 50,
+        "Question": "Which foods trigger eczema? 哪些食物會引發濕疹？",
+        "Evidence": [
+            "1: The common offenders are: Milk Eggs Peanuts Tree nuts Wheat Fish Shellfish Soy While trigger foods can make eczema worse, experts don't think they're really the original cause. 常見的罪魁禍首是： 牛奶、雞蛋、花生、堅果、小麥、魚類、貝類、大豆 雖然觸發食物會使濕疹惡化，但專家認為它們並不是真正的根源。",
+            "2: Instead, it seems to result from 'leakiness' in the outer layer of skin that lets in irritants, germs, and allergens. 相反，它似乎是由於皮膚外層「滲漏」導致刺激物、細菌和過敏原進入。",
+            "3: Some are obvious. 有些是顯而易見的。"
+        ],
+        "Answer": "Milk, eggs, peanuts, tree nuts, wheat, fish, shellfish, and soy are common food triggers for eczema. However, experts believe that these foods are not the original cause of eczema, but rather they can make eczema worse by causing inflammation and irritation. 牛奶、雞蛋、花生、堅果、小麥、魚、貝類和大豆是濕疹的常見食物誘因。然而，專家認為，這些食物並不是濕疹的根源，而是會引起發炎和刺激，從而使濕疹惡化。"
+    },
+    {
+        # 42,
+        "Question": "What can someone do to help their rheumatoid arthritis? 人們可以做些什麼來幫助治療類風濕性關節炎？",
+        "Evidence": [
+            "1: It might sound crazy at first, but a workout can boost your energy and make you more flexible.乍聽之下可能很瘋狂，但運動可以增強您的能量並讓您更加靈活。",
+            "2: The next day, try to do a minute more, and so on. 第二天，試著多做一分鐘，依此類推。"
+        ],
+        "Answer": "Exercise can help improve energy levels and flexibility in people with rheumatoid arthritis. Start with a simple workout and gradually increase the duration and intensity over time. 運動有助於提高類風濕性關節炎患者的能量水平和靈活性。從簡單的運動開始，隨著時間的推移逐漸增加持續時間和強度。"
+    },
+    {
+        # 16,
+        "Question": "Why is it important to treat Lyme disease? 為什麼治療萊姆病很重要？",
+        "Evidence": [
+            "1: The signs and symptoms can look like many other health problems. 體徵和症狀可能看起來像許多其他健康問題。",
+            "2: It can even affect your memory. 它甚至會影響你的記憶力。"
+        ],
+        "Answer": "Lyme disease can be difficult to diagnose because its symptoms can mimic those of other illnesses. Additionally, the infection can cause neurological problems, including memory loss. Therefore, it is important to seek medical attention if you suspect you may have Lyme disease. 萊姆病可能很難診斷，因為它的症狀與其他疾病相似。此外，感染也會導致神經系統問題，包括記憶喪失。因此，如果您懷疑自己可能患有萊姆病，請務必就醫。"
+    }
+]
+
+questions = [
+    {
+        "text": "Question 1: 答案(Answer)是否只包含證據(Evidence)中提到的資訊? (亦即是否有超出證據的資訊)",
+        "options": ["1: 完全符合", "2: 大部分符合", "3: 少部分符合", "4: 完全不符合"],
+        "options_note": ["完全符合 (答案完全依循證據中的資訊，沒有任何超出證據的額外資訊。換言之，答案提及的資訊都可以在證據中找到)。\
+                         [補充: 這包含用詞不同但意義上相同的答案，或者為了使回答更流暢而加入的詞句並不算超出證據]",
+                         "大部分符合 (答案大部分依循證據中的資訊，但可能加入了一些輔助資訊或解釋。\
+                            這些輔助資訊並不改變證據的主要含義，但可能增加了一些背景或細節，並且這些細節是正確的。)",
+                         "部分符合 (答案只有部分根據證據中的資訊，其他大部分內容是由 LLM 加入的額外資訊或解釋，這些資訊可能是證據的延伸或無關，\
+                            但無法保證這些延伸的正確性)",
+                         "完全不符合 (答案完全沒有根據證據中的資訊，內容都是額外資訊、錯誤資訊或與證據無關的內容。)"]
+    },
+    {
+        "text": "Question 2: 答案(Answer)是否涵蓋所有證據(Evidence)中提及的資訊?",
+        "options": ["1: 完全涵蓋","2: 大部分涵蓋","3: 部分涵蓋", "4: 完全未涵蓋"],
+        "options_note": ["完全涵蓋 (答案的內容涵蓋了證據中每個提及的重要資訊，這些資訊在答案中可能經過重寫、精簡、總結，但意思不變。)",
+                            "大部分涵蓋 (答案內容涵蓋證據中的大部分片段的重要資訊，\
+                                但可能有些未包含，其中未包含的片段為可省略的次要或無關資訊。)",
+                            "部分涵蓋 (答案中只有一部分重要資訊被涵蓋，大部分證據片段並未在答案被提及，代表有重要的片段被忽略了。)",
+                            "完全未涵蓋 (答案中完全沒有涵蓋證據中的任何句子所提及的重要資訊。)"]
+    },
+    {
+        "text": "Question 3: 答案(Answer)的表達是否通順且流利?",
+        "options": ["1: 非常通順","2: 較為通順","3: 不太通順", "4: 非常不通順"],
+        "options_note": ["非常通順 (答案表達流暢、通順。)",
+                            "較為通順 (答案表達大致流暢，但可能有少量語法錯誤或不自然的表達方式，但不影響整體理解。)",
+                            "不太通順 (答案表達不太流暢，有較多的語法錯誤或不自然的地方，影響了理解。)",
+                            "非常不通順 (答案表達不流暢，具有語法錯誤與不自然的表達，難以理解。)"]
+    },
+    {
+        "text": "Question 4: 答案 (Answer) 的內容是否正確?",
+        "options": ["1: 完全正確","2: 大部分正確","3: 部分正確", "4: 完全不正確"],
+        "options_note": ["完全正確 (獨立於其他題目，假設Evidence為Ground truth (正解) 的情況下，答案中的資訊正確，與證據或已知事實一致。或者基於你的知識背景，答案中的資訊是正確的。)",
+                         "大部分正確 (獨立於其他題目，假設Evidence為Ground truth (正解) 的情況下，答案中的大部分資訊是正確的，但可能有一些小錯誤或不準確的地方。)",
+                         "部分正確 (獨立於其他題目，假設Evidence為Ground truth (正解) 的情況下，答案中只有一部分資訊是正確的，大部分內容有誤。)",
+                         "完全不正確 (獨立於其他題目，假設Evidence為Ground truth (正解) 的情況下，答案中的所有資訊都是錯誤的，沒有一部分是正確的。)"]
+    }
+]
+
+def get_contexts():
+    return Contexts
+
+def get_questions():
+    return questions
